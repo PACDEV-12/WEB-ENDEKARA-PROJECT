77252640c4a04246a7328f612ea61e407ab83cae
PACDEV-12 <mahardaya@undiksha.ac.id>
2024-12-05 00:35:39 +0700

    commit
    gas

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-  plugins: [
-    laravel({
-      input: ['resources/css/app.css', 'resources/js/app.js'],
-      refresh: true,
-    }),
-  ],
+    build: {
+        outDir: 'public/dist',  // Menyimpan hasil build ke dalam folder public/dist
+    },
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
 });
